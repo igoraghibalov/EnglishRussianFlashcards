e592c23d89def13f32cada424a217494cda8466e
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-04-24 22:10:57 +0300

    Refactoring: add setup() method to MainScreenUiTester class

--- app/src/androidTest/java/com/example/englishrussianflashcards/MainScreenUiTester.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/MainScreenUiTester.kt
@@ -1,11 +1,16 @@
 package com.example.englishrussianflashcards
 
+import android.app.Application
+import android.content.Intent
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -14,6 +19,14 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class MainScreenUiTester: UiTester {
+
+
+    @Before
+    fun setup() {
+        val applicationContext = ApplicationProvider.getApplicationContext<Application>()
+        val intent = Intent(applicationContext,MainActivity::class.java)
+        ActivityScenario.launch<MainActivity>(intent)
+    }
 
 
     override fun clickOnView(viewId: Int) {
