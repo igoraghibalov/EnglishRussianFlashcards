48f50251c6d3d1edf9c90b045a9bf56eef7e1e51
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-04-25 22:47:09 +0300

    Refactoring: change interface UiTester to abstract class

--- app/src/androidTest/java/com/example/englishrussianflashcards/UiTester.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/UiTester.kt
@@ -1,10 +1,33 @@
 package com.example.englishrussianflashcards
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+
 /**
  * Created by Igor Aghibalov on 22.04.2024
  */
-interface UiTester {
-    fun clickOnView(viewId: Int)
-    fun rotateScreen()
-    fun checkViewAppearance(viewId: Int)
+abstract class UiTester {
+
+    private lateinit var mainActivityScenario: ActivityScenario<MainActivity>
+
+    fun clickOnView(viewId: Int) {
+        Espresso.onView(ViewMatchers.withId(viewId))
+            .perform(ViewActions.click())
+    }
+
+    abstract fun rotateScreen()
+
+    fun checkViewAppearance(viewId: Int) {
+        Espresso.onView(ViewMatchers.withId(viewId))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    fun testFragmentInflation(baseLayoutViewId: Int, inflatedFragmentViewId: Int) {
+        clickOnView(baseLayoutViewId)
+        checkViewAppearance(inflatedFragmentViewId)
+        mainActivityScenario.recreate()
+    }
 }
