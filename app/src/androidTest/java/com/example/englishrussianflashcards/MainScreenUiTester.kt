758d581049c0af7a290e93041ad529be98fbfc3c
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-04-27 19:34:00 +0300

    Refactoring: put setup() method of MainScreenUiTester in a base abstract class UiTester

--- app/src/androidTest/java/com/example/englishrussianflashcards/MainScreenUiTester.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/MainScreenUiTester.kt
@@ -4,16 +4,8 @@ import android.app.Application
 import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.Matchers.not
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 
 /**
  * Created by Igor Aghibalov on 22.04.2024
@@ -22,12 +14,6 @@ class MainScreenUiTester: UiTester() {
 
     private lateinit var mainActivityScenario: ActivityScenario<MainActivity>
 
-    @Before
-    fun setup() {
-        val applicationContext = ApplicationProvider.getApplicationContext<Application>()
-        val intent = Intent(applicationContext, MainActivity::class.java)
-        mainActivityScenario = ActivityScenario.launch<MainActivity>(intent)
-    }
 
     override fun rotateScreen() {
         TODO("Not yet implemented")
@@ -37,7 +23,7 @@ class MainScreenUiTester: UiTester() {
     fun testFragmentInflationsOnMainMenuButtonsClicks() {
         testFragmentInflation(R.id.new_button, R.id.word_typing_text_view)
         testFragmentInflation(R.id.continue_button, R.id.flashcard_front_side_view)
-        testFragmentInflation(R.id.cards_button, R.id.card_recycler_view)
+        testFragmentInflation(R.id.cards_button, R.id.card_group_recycler_view)
         testFragmentInflation(R.id.history_button, R.id.card_history_recycler_view)
     }
 }
--- app/src/androidTest/java/com/example/englishrussianflashcards/UiTester.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/UiTester.kt
@@ -1,10 +1,14 @@
 package com.example.englishrussianflashcards
 
+import android.app.Application
+import android.content.Intent
 import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import org.junit.Before
 
 /**
  * Created by Igor Aghibalov on 22.04.2024
@@ -12,6 +16,13 @@ import androidx.test.espresso.matcher.ViewMatchers
 abstract class UiTester {
 
     private lateinit var mainActivityScenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setup() {
+        val applicationContext = ApplicationProvider.getApplicationContext<Application>()
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        mainActivityScenario = ActivityScenario.launch<MainActivity>(intent)
+    }
 
     fun clickOnView(viewId: Int) {
         Espresso.onView(ViewMatchers.withId(viewId))
