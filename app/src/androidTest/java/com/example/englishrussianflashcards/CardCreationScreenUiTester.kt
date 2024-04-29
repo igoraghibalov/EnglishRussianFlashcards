ef7a23ca54a52af05fb644039b71aec49fcbcb2a
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-04-29 23:33:57 +0300

    New feature: add CardCreationScreenUiTester class

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/CardCreationScreenUiTester.kt
@@ -0,0 +1,43 @@
+package com.example.englishrussianflashcards
+
+import android.graphics.drawable.Drawable
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.instanceOf
+import org.junit.Test
+
+/**
+ * Created by Igor Aghibalov on 29.04.2024
+ */
+class CardCreationScreenUiTester: UiTester() {
+
+    //TODO: refactor line 21,22 code duplication
+    @Test
+    fun testCardCreationDialogFragmentInflationOnCreateButtonClick() {
+        val defaultTypingCharSequence = applicationContext.resources.getString(R.string.default_typing_char_sequence)
+        val defaultWordGroupName = applicationContext.resources.getString(R.string.default_word_group_name)
+
+        testFragmentInflation(R.id.new_button, R.id.word_typing_text_view)
+
+        onView(withId(R.id.word_typing_text_view)).perform(typeText(defaultTypingCharSequence))
+
+        onView(withId(R.id.translation_selection_text_view)).perform(click())
+        onData(allOf(instanceOf(String::class.java))).atPosition(0).perform(click())
+
+        onView(withId(R.id.english_example_selection_text_view)).perform(click())
+        onData(allOf(instanceOf(String::class.java))).atPosition(0).perform(click())
+
+        onView(withId(R.id.image_selection_text_view)).perform(click())
+        onData(allOf(instanceOf(Drawable::class.java))).atPosition(0).perform(click())
+
+        onView(withId(R.id.word_typing_text_view)).perform(typeText(defaultWordGroupName))
+
+        testFragmentInflation(R.id.card_creation_button, R.id.flashcard_front_side_view)
+    }
+
+
+}
