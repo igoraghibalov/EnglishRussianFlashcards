b15ab6f8e342c48eac86c2fc88c73cc3828a3752
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-05-01 22:17:46 +0300

    New feature: add HistoryScreenUiTester class

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/HistoryScreenUiTester.kt
@@ -0,0 +1,26 @@
+package com.example.englishrussianflashcards
+
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.Matchers.instanceOf
+import org.junit.Test
+
+/**
+ * Created by Igor Aghibalov on 01.05.2024
+ */
+
+
+class HistoryScreenUiTester: UiTester() {
+
+
+    @Test
+    fun testCardFragmentInflationOnHistoryListItemClick() {
+        testFragmentInflation(R.id.history_button, R.id.card_history_recycler_view)
+        onView(withId(R.id.card_history_recycler_view)).perform(click())
+        onData(allOf(instanceOf(String::class.java))).atPosition(0).perform(click())
+        checkViewAppearance(R.id.flashcard_front_side_view)
+    }
+}
