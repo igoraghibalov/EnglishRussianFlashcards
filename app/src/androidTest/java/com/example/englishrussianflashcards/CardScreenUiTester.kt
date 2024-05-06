035d3565143d3087d4a073e76bbe5fa35ee51d8c
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-05-06 22:54:41 +0300

    Refactoring: add testCardRotationOnClick() method to CardScreenUiTester class

--- app/src/androidTest/java/com/example/englishrussianflashcards/CardScreenUiTester.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/CardScreenUiTester.kt
@@ -6,6 +6,7 @@ import androidx.cardview.widget.CardView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.swipeLeft
 import androidx.test.espresso.action.ViewActions.swipeRight
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -23,6 +24,7 @@ import org.junit.Test
  */
 class CardScreenUiTester: UiTester() {
 
+
     override fun setup() {
         super.setup()
         testFragmentInflation(R.id.continue_button, R.id.card_front_side_view)
@@ -37,6 +39,13 @@ class CardScreenUiTester: UiTester() {
         onView(withText(initialCardWord)).check(matches(not(isDisplayed())))
         cardViewInteraction.perform(swipeRight())
         onView(withText(initialCardWord)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testCardRotationOnClick() {
+        val cardViewInteraction = onView(withId(R.id.card_front_side_view))
+        cardViewInteraction.perform(click())
+        onView(withId(R.id.word_image_view)).check(matches(isDisplayed()))
     }
 
 
@@ -59,6 +68,4 @@ class CardScreenUiTester: UiTester() {
         cardViewInteraction.perform(cardWordExtractionAction)
         return cardWord
     }
-
-
 }
