5a0a13008a21dd573bcac808a94527675fb7b52a
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-05-09 23:49:08 +0300

    Refactoring: append coroutines to testCardListExtraction() of MainScreenViewModelTester class

--- app/src/test/java/com/example/englishrussianflashcards/MainScreenViewModelTester.kt
+++ app/src/test/java/com/example/englishrussianflashcards/MainScreenViewModelTester.kt
@@ -2,17 +2,24 @@ package com.example.englishrussianflashcards
 
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.runner.AndroidJUnitRunner
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
-
 /**
  * Created by Igor Aghibalov on 08.05.2024
  */
 class MainScreenViewModelTester {
     private lateinit var fakeCardList: List<Card>
+
 
     @Before
     fun setup() {
@@ -27,8 +34,9 @@ class MainScreenViewModelTester {
         val fakeCardRepository = FakeCardRepository()
         val mainScreenViewModel = MainScreenViewModel(fakeCardRepository)
 
-        //TODO: use coroutines
-        mainScreenViewModel.extractCardList()
+        runTest {
+            mainScreenViewModel.extractCardList()
+        }
 
         assertEquals(true, mainScreenViewModel.hasList(fakeCardList))
     }
