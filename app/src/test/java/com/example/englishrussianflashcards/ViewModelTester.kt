d828882fa7e4e078e9cccc4f1e1710d1ad08f4aa
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-05-11 23:35:37 +0300

    Refactoring: add fakeSavedStateHandle property to ViewModelTester

--- app/src/test/java/com/example/englishrussianflashcards/ViewModelTester.kt
+++ app/src/test/java/com/example/englishrussianflashcards/ViewModelTester.kt
@@ -1,5 +1,6 @@
 package com.example.englishrussianflashcards
 
+import androidx.lifecycle.SavedStateHandle
 import org.junit.Before
 
 /**
@@ -8,6 +9,7 @@ import org.junit.Before
 abstract class ViewModelTester {
     protected lateinit var fakeCardRepository: CardRepository
     protected lateinit var viewModel: FlashcardsApplicationViewModel
+    protected lateinit var fakeSavedStateHandle: SavedStateHandle
 
     @Before
     abstract fun setup()
