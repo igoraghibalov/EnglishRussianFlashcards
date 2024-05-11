4db4fed7dc6774c4849272fc4631b593bdc064bc
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-05-11 23:15:10 +0300

    Refactoring: include ViewPager2 ViewGroup in a card_screen_layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.test.espresso:espresso-contrib:3.5.1")
     implementation("androidx.test.uiautomator:uiautomator:2.3.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1-Beta")
-    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1-Betas")
+    testImplementation (libs.kotlinx.coroutines.test)
 //    implementation("androidx.activity:activity-compose:1.8.2")
 //    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
 //    implementation("androidx.compose.ui:ui")
