998d46baad3f8b9331b26cbafc05ef72e3f1fd43
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-05-12 14:13:44 +0300

    Refactoring: inject fakeCardList as initial state into SaveStateHandle in a setup() method of a CardScreenViewModelTester class

--- app/src/test/java/com/example/englishrussianflashcards/CardScreenViewModelTester.kt
+++ app/src/test/java/com/example/englishrussianflashcards/CardScreenViewModelTester.kt
@@ -1,5 +1,6 @@
 package com.example.englishrussianflashcards
 
+import androidx.core.os.bundleOf
 import androidx.lifecycle.SavedStateHandle
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
@@ -15,12 +16,12 @@ class CardScreenViewModelTester: ViewModelTester() {
 
     @Before
     override fun setup() {
-        fakeCardRepository = FakeCardRepository()
-        fakeSavedStateHandle = SavedStateHandle(mapOf("word" to "apple"))
-        viewModel = CardScreenViewModel(fakeCardRepository, fakeSavedStateHandle)
         val fakeWord = "apple"
         val fakeTranslation = "яблоко"
         fakeCardList = listOf(Card(fakeWord, fakeTranslation))
+        fakeCardRepository = FakeCardRepository()
+        fakeSavedStateHandle = SavedStateHandle(mapOf("cardList" to fakeCardList))
+        viewModel = CardScreenViewModel(fakeCardRepository, fakeSavedStateHandle)
     }
 
 
