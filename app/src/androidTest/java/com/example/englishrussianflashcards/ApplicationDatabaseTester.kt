d4f7e78dfc357dbe7700c1a6256ae95794da4392
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-05-20 23:25:44 +0300

    New feature: add ApplicationDatabaseTester class

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/ApplicationDatabaseTester.kt
@@ -0,0 +1,57 @@
+package com.example.englishrussianflashcards
+
+import android.app.Application
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * Created by Igor Aghibalov on 20.05.2024
+ */
+@RunWith(AndroidJUnit4::class)
+class ApplicationDatabaseTester {
+    private lateinit var applicationDatabase: ApplicationDatabase
+    private lateinit var cardDao: CardDao
+    private lateinit var additionalTestCoroutineContext: CoroutineContext
+
+    @Before
+    fun setupDatabaseEnvironment() {
+        val databaseContext = ApplicationProvider.getApplicationContext<Application>()
+        applicationDatabase = Room.inMemoryDatabaseBuilder(databaseContext, ApplicationDatabase::class.java).build()
+        cardDao = applicationDatabase.getCardDao()
+        additionalTestCoroutineContext = Dispatchers.IO
+    }
+
+    @Test
+    fun testSuccessCardListExtraction() {
+
+        runBlocking {
+            val fakeWord = "apple"
+            val fakeTranslation = "яблоко"
+            val isDisplayed = true
+            val fakeCard = Card(fakeWord, fakeTranslation, isDisplayed)
+            val fakeCardList = listOf(fakeCard)
+
+            val cardInsertionJob = launch(additionalTestCoroutineContext) { cardDao.insertCard(fakeCard) }
+            cardInsertionJob.join()
+            val cardList = withContext(additionalTestCoroutineContext) { cardDao.getCardList() }
+            assertEquals(true, cardList == fakeCardList)
+        }
+    }
+
+
+    @After
+    fun closeDatabase() {
+        applicationDatabase.close()
+    }
+}
