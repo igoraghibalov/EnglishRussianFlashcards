b5e8c859c99360d8efca787e9066b107d222e94c
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-05-29 22:47:07 +0300

    Refactoring: set quitButton click listener in the onViewCreated() method of  MainMenuFragment class

--- app/src/main/java/com/example/englishrussianflashcards/presentation/MainMenuFragment.kt
+++ app/src/main/java/com/example/englishrussianflashcards/presentation/MainMenuFragment.kt
@@ -1,6 +1,7 @@
 package com.example.englishrussianflashcards.presentation
 
 import android.os.Bundle
+import android.os.Process
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -42,6 +43,10 @@ class MainMenuFragment: Fragment() {
                                             mainMenuViewBinding.continueButton to "CardHistoryFragment")
 
         setFragmentAppendingTransactionOnButtonClick(appendedFragmentNameMap)
+
+        mainMenuViewBinding.quitButton.setOnClickListener {
+            Process.killProcess(Process.myPid())
+        }
     }
 
 
