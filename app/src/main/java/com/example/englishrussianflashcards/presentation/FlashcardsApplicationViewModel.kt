da207f48829253b271dd4a271586049be35b2745
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-05-29 23:38:22 +0300

    Refactoring: set TODO comment: "add SavedStateHandle class dependency", - to FlashcardsApplicationViewModel class

--- app/src/main/java/com/example/englishrussianflashcards/presentation/FlashcardsApplicationViewModel.kt
+++ app/src/main/java/com/example/englishrussianflashcards/presentation/FlashcardsApplicationViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 /**
  * Created by Igor Aghibalov on 29.05.2024
  */
+//TODO: append SavedStateHandle class dependency
 abstract class FlashcardsApplicationViewModel<T: Any>(private val observableContainerMap: Map<String, ObservableContainer<T>>,
                                                       private val repository: Repository): ViewModel(),
                                                                                            ObservableContainerSubscriber {
