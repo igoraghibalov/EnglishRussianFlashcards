457686a49aec396610ec8a3df2c1ae08af386517
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-05-30 23:40:22 +0300

    New feature: add DictionaryEntryExtractor interface

---
+++ app/src/main/java/com/example/englishrussianflashcards/domain/DictionaryEntryExtractor.kt
@@ -0,0 +1,12 @@
+package com.example.englishrussianflashcards.domain
+
+/**
+ * Created by Igor Aghibalov on 30.05.2024
+ */
+interface DictionaryEntryExtractor {
+    fun getWordList(): List<String>
+    fun getTranslationList(): List<String>
+    fun getTranscription(): String
+    fun getExampleList(): List<String>
+    fun getGroupTitleList(): List<String>
+}
