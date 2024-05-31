a62055469742f0c7ae1ca77cd997484ec856d698
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-05-31 22:48:50 +0300

    New feature: add Repository abstract class

---
+++ app/src/main/java/com/example/englishrussianflashcards/domain/Repository.kt
@@ -0,0 +1,11 @@
+package com.example.englishrussianflashcards.domain
+
+
+
+/**
+ * Created by Igor Aghibalov on 30.05.2024
+ */
+abstract class Repository(private val dataSource: DataSource) {
+}
+
+
