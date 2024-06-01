05707dfbe8f4e1ec2021831d1174ca760dfb53f4
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-01 21:16:51 +0300

    Refactoring: make ObservableContainerSubscriber interface generic and make subscribeObserverToContainerValueChange() method observer parameter generic type as covariant

--- app/src/main/java/com/example/englishrussianflashcards/presentation/ObservableContainerSubscriber.kt
+++ app/src/main/java/com/example/englishrussianflashcards/presentation/ObservableContainerSubscriber.kt
@@ -6,9 +6,9 @@ import androidx.lifecycle.Observer
 /**
  * Created by Igor Aghibalov on 29.05.2024
  */
-interface ObservableContainerSubscriber {
+interface ObservableContainerSubscriber<T> {
 
-    fun <T> subscribeObserverToContainerValueChange(observableContainerTag: String,
-                                                    lifecycleOwner: LifecycleOwner,
-                                                    observer: Observer<T>)
+    fun subscribeObserverToContainerValueChange(observableContainerTag: String,
+                                                lifecycleOwner: LifecycleOwner,
+                                                observer: Observer<out T>)
 }
