ebbe52df4007eaaf15179ec0b2314072c257c4a5
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-01 20:03:24 +0300

    Refactoring: change mutableLiveData property access modifier from private to protected in an ObservableContainer abstract class

--- app/src/main/java/com/example/englishrussianflashcards/presentation/ObservableContainer.kt
+++ app/src/main/java/com/example/englishrussianflashcards/presentation/ObservableContainer.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.Observer
 /**
  * Created by Igor Aghibalov on 29.05.2024
  */
-abstract class ObservableContainer <T: Any> (private val mutableLiveData: MutableLiveData<T>) {
+abstract class ObservableContainer <T: Any> (protected val mutableLiveData: MutableLiveData<T>) {
     abstract fun setObserver(lifecycleOwner: LifecycleOwner, observer: Observer<T>)
     abstract fun getValue(): T
     abstract fun setValue(newValue: T)
