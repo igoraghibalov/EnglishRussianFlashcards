036f51ea045418439cc29754ae076ee2010dcbc8
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-02 19:53:12 +0300

    New feature: create CardLearningFragment class

---
+++ app/src/main/java/com/example/englishrussianflashcards/presentation/CardLearningFragment.kt
@@ -0,0 +1,16 @@
+package com.example.englishrussianflashcards.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import com.example.englishrussianflashcards.R
+import com.example.englishrussianflashcards.databinding.CardScreenLayoutBinding
+
+/**
+ * Created by Igor Aghibalov on 31.05.2024
+ */
+class CardLearningFragment: Fragment(R.layout.card_screen_layout)
