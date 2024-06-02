5069ae684560eefb37c981738b323452b9ed12e7
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-02 19:47:05 +0300

    New feature: add CardCreationFragment class

---
+++ app/src/main/java/com/example/englishrussianflashcards/presentation/CardCreationFragment.kt
@@ -0,0 +1,9 @@
+package com.example.englishrussianflashcards.presentation
+
+import androidx.fragment.app.Fragment
+import com.example.englishrussianflashcards.R
+
+/**
+ * Created by Igor Aghibalov on 02.06.2024
+ */
+class CardCreationFragment: Fragment(R.layout.card_creation_screen_layout)
