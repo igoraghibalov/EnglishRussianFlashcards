151874d6bbdda820d822f22e27a9482f022ce61d
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-02 19:56:56 +0300

    New feature: create CardHistoryFragment class

---
+++ app/src/main/java/com/example/englishrussianflashcards/presentation/CardHistoryFragment.kt
@@ -0,0 +1,10 @@
+package com.example.englishrussianflashcards.presentation
+
+import androidx.fragment.app.Fragment
+import com.example.englishrussianflashcards.R
+
+/**
+ * Created by Igor Aghibalov on 02.06.2024
+ */
+class CardHistoryFragment: Fragment(R.layout.history_screen_layout) {
+}
