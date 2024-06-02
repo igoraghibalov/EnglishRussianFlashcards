a875d13486e1cb716309fca4b747ad9730fea6cc
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-02 19:52:21 +0300

    Refactoring: replace 'R.id.card_front_side_view' with 'R.id.card_view_pager' as  testFragmentInflation() method parameter in a MainScreenUiTester class

--- app/src/androidTest/java/com/example/englishrussianflashcards/MainScreenUiTester.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/MainScreenUiTester.kt
@@ -16,7 +16,7 @@ class MainScreenUiTester: UiTester() {
     @Test
     fun testFragmentInflationsOnMainMenuButtonsClicks() {
         testFragmentInflation(R.id.new_button, R.id.word_typing_text_view)
-        testFragmentInflation(R.id.continue_button, R.id.card_front_side_view)
+        testFragmentInflation(R.id.continue_button, R.id.card_view_pager)
         testFragmentInflation(R.id.cards_button, R.id.card_group_recycler_view)
         testFragmentInflation(R.id.history_button, R.id.card_history_recycler_view)
     }
