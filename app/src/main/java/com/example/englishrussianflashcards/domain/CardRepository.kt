f34093255affe1913805d7a87198ac8288e81125
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-05 23:55:29 +0300

    New feature: create CardRepository class

---
+++ app/src/main/java/com/example/englishrussianflashcards/domain/CardRepository.kt
@@ -0,0 +1,49 @@
+package com.example.englishrussianflashcards.domain
+
+import kotlinx.coroutines.CloseableCoroutineDispatcher
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+/**
+ * Created by Igor Aghibalov on 05.06.2024
+ */
+class CardRepository(applicationDatabase: ApplicationDatabase,
+                     private val coroutineDispatcher: CoroutineDispatcher)
+    : Repository(applicationDatabase), CardExtractor {
+    private val cardDao: CardDao
+
+    init {
+        cardDao = (dataSource as ApplicationDatabase).getCardDao()
+    }
+
+    override suspend fun getCardList(): List<Card> {
+
+        return withContext(coroutineDispatcher) {
+            cardDao.getCardList()
+        }
+    }
+
+
+    override suspend fun getCardWordMap(): Map<String, String> {
+
+        return withContext(coroutineDispatcher) {
+            cardDao.getWordMap()
+        }
+    }
+
+
+    override suspend fun getCardGroupMap(): Map<String, List<Card>> {
+        return withContext(coroutineDispatcher) {
+            cardDao.getCardGroupMap()
+        }
+    }
+
+
+    override suspend fun getGroupTitleList(): List<String> {
+        return withContext(coroutineDispatcher) {
+            cardDao.getGroupTitleList()
+        }
+    }
+}
