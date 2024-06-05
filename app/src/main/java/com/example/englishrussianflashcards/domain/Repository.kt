c21f82267cd8065237bcf1751c901888d88fb51e
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-05 23:54:29 +0300

    Refactoring: change Repository dataSource property access modifier from private to protected

--- app/src/main/java/com/example/englishrussianflashcards/domain/Repository.kt
+++ app/src/main/java/com/example/englishrussianflashcards/domain/Repository.kt
@@ -5,7 +5,7 @@ package com.example.englishrussianflashcards.domain
 /**
  * Created by Igor Aghibalov on 30.05.2024
  */
-abstract class Repository(private val dataSource: DataSource) {
+abstract class Repository(protected val dataSource: DataSource) {
 }
 
 
