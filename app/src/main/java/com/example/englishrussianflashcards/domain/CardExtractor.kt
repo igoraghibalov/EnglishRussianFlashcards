a5e55a0e1d409d1ad7047fd9d401d24ad980a14b
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-05 23:39:40 +0300

    Refactoring: make CardExtractor interface methods suspend

--- app/src/main/java/com/example/englishrussianflashcards/domain/CardExtractor.kt
+++ app/src/main/java/com/example/englishrussianflashcards/domain/CardExtractor.kt
@@ -4,8 +4,8 @@ package com.example.englishrussianflashcards.domain
  * Created by Igor Aghibalov on 30.05.2024
  */
 interface CardExtractor {
-    fun getCardList(): List<Card>
-    fun getCardWordMap(): Map<String, String>
-    fun getCardGroupMap(): Map<String, List<Card>>
-    fun getGroupTitleList(): List<String>
+    suspend fun getCardList(): List<Card>
+    suspend fun getCardWordMap(): Map<String, String>
+    suspend fun getCardGroupMap(): Map<String, List<Card>>
+    suspend fun getGroupTitleList(): List<String>
 }
