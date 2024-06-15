d4be4056a1271a50a590824191e7a3f4c04c1b81
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-15 17:14:19 +0300

    New feature: add FragmentInflationTestHandler interface

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/FragmentInflationTestHandler.kt
@@ -0,0 +1,9 @@
+package com.example.englishrussianflashcards
+
+/**
+ * Created by Igor Aghibalov on 15.06.2024
+ */
+interface FragmentInflationTestHandler {
+    fun handleTest()
+    fun doAfterFragmentInflationTest()
+}
