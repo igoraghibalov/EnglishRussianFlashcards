9456aac5861013e1114a2e02e2d24926d5d97b60
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-15 17:21:40 +0300

    Refactoring: simplify testFragmentInflationsOnMainMenuButtonsClicks() method of the MainScreenUiTester class

--- app/src/androidTest/java/com/example/englishrussianflashcards/MainScreenUiTester.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/MainScreenUiTester.kt
@@ -1,24 +1,28 @@
 package com.example.englishrussianflashcards
 
-import androidx.test.espresso.Espresso
 import org.junit.Test
 
 /**
  * Created by Igor Aghibalov on 22.04.2024
  */
 class MainScreenUiTester: UiTester() {
+    private lateinit var clickedViewToInflatedFragmentViewMap: Map<Int, Int>
+    private lateinit var fragmentInflationTestHandler: FragmentInflationTestHandler
 
-
-    override fun testFragmentInflation(baseLayoutViewId: Int, inflatedFragmentViewId: Int) {
-        super.testFragmentInflation(baseLayoutViewId, inflatedFragmentViewId)
-        Espresso.pressBack()
+    override fun setup() {
+        super.setup()
+        clickedViewToInflatedFragmentViewMap = mapOf(R.id.new_button to R.id.word_typing_text_view,
+                                                     R.id.continue_button to R.id.card_view_pager,
+                                                     R.id.cards_button to R.id.card_group_recycler_view,
+                                                     R.id.history_button to R.id.card_history_recycler_view)
+        fragmentInflationTestHandler = MainScreenFragmentsInflationTestHandler(clickedViewToInflatedFragmentViewMap)
     }
+
 
     @Test
     fun testFragmentInflationsOnMainMenuButtonsClicks() {
-        testFragmentInflation(R.id.new_button, R.id.word_typing_text_view)
-        testFragmentInflation(R.id.continue_button, R.id.card_view_pager)
-        testFragmentInflation(R.id.cards_button, R.id.card_group_recycler_view)
-        testFragmentInflation(R.id.history_button, R.id.card_history_recycler_view)
+        testFragmentInflation(fragmentInflationTestHandler)
     }
+
+
 }
