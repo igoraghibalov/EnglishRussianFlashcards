4ff8e1d580243d175e072aa992b59acd8740adc4
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-16 17:10:37 +0300

    New feature: add CaseTestHandler interface

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/CaseTestHandler.kt
@@ -0,0 +1,9 @@
+package com.example.englishrussianflashcards
+
+/**
+ * Created by Igor Aghibalov on 16.06.2024
+ */
+interface CaseTestHandler {
+    fun handleTestCase()
+    fun doAfterCaseTestHandling()
+}
