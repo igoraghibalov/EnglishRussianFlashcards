f135d8d1da0db162328c8fa4ca1ee38c018ac1af
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-16 17:13:43 +0300

    Refactoring: implement (TestEnvironmentSetuper, CaseTester, ScreenRotator) interfaces by UiTester abstract class

--- app/src/androidTest/java/com/example/englishrussianflashcards/UiTester.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/UiTester.kt
@@ -2,13 +2,8 @@ package com.example.englishrussianflashcards
 
 import android.app.Application
 import android.content.Context
-import android.os.Build
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.UiDevice
 import com.example.englishrussianflashcards.presentation.MainActivity
@@ -17,31 +12,22 @@ import org.junit.Before
 /**
  * Created by Igor Aghibalov on 22.04.2024
  */
-abstract class UiTester {
+abstract class UiTester: TestEnvironmentSetuper, CaseTester, ScreenRotator {
 
     protected lateinit var mainActivityScenario: ActivityScenario<MainActivity>
     protected val applicationContext: Context = ApplicationProvider.getApplicationContext<Application>()
 
     @Before
-    open fun setup() {
+    override fun setupTestEnvironment() {
         mainActivityScenario = ActivityScenario.launch(MainActivity::class.java)
     }
 
-    fun setupProcessDeathTestEnvironment() {
-        val application = applicationContext as Application
-        val isSdkPrePie = application.applicationInfo.targetSdkVersion < Build.VERSION_CODES.P
-        val processTerminator = EnglishRussianFlashcardsApplicationProcessTerminator()
-        val mainActivityLifecycleObserver = MainActivityLifecycleObserver(isSdkPrePie, processTerminator)
-        application.registerActivityLifecycleCallbacks(mainActivityLifecycleObserver)
+    override fun testCase(caseTestHandler: CaseTestHandler) {
+        caseTestHandler.handleTestCase()
     }
 
 
-    fun rotateScreen() {
+    override fun rotateScreen() {
         UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).setOrientationLeft()
-    }
-
-
-    fun testFragmentInflation(fragmentInflationTestHandler: FragmentInflationTestHandler) {
-        fragmentInflationTestHandler.handleTest()
     }
 }
