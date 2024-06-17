4e8d1314465b2814d0707a0eacf4e5d7ac203a2a
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-17 22:30:49 +0300

    Refactoring: extend new CaseTestHandler abstract class by MainScreenFragmentsInflationTestHandler

--- app/src/androidTest/java/com/example/englishrussianflashcards/MainScreenFragmentsInflationTestHandler.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/MainScreenFragmentsInflationTestHandler.kt
@@ -4,36 +4,25 @@ import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import org.junit.Test
 
 /**
  * Created by Igor Aghibalov on 15.06.2024
  */
-
 class MainScreenFragmentsInflationTestHandler(private val clickedViewToInflatedFragmentViewMap: Map<Int, Int>)
-    : FragmentInflationTestHandler, ViewClickHandler, ViewAppearanceTester {
+        : CaseTestHandler() {
 
-    override fun handleTest() {
+
+    override fun handleCaseTest() {
 
         clickedViewToInflatedFragmentViewMap.forEach {
             clickOnView(it.key)
             checkViewAppearance(it.value)
-            doAfterFragmentInflationTest()
         }
     }
 
 
-    override fun doAfterFragmentInflationTest() {
+    override fun doAfterCaseTestHandling() {
         Espresso.pressBack()
     }
-
-    override fun clickOnView(viewId: Int) {
-        Espresso.onView(ViewMatchers.withId(viewId))
-            .perform(ViewActions.click())
-    }
-
-    override fun checkViewAppearance(viewId: Int) {
-        Espresso.onView(ViewMatchers.withId(viewId))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-    }
-
 }
