31d921dc0aa2e33d7de8ff7cdf6fd3e2e0867cf3
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-18 23:28:45 +0300

    Refactoring: remove hardcoded resources from ApplicationDatabaseTester class

--- app/src/androidTest/java/com/example/englishrussianflashcards/ApplicationDatabaseTester.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/ApplicationDatabaseTester.kt
@@ -32,15 +32,52 @@ class ApplicationDatabaseTester {
     private lateinit var cardDao: CardDao
     private lateinit var dictionaryDao: DictionaryDao
     private lateinit var additionalTestCoroutineContext: CoroutineContext
+    private lateinit var appleCard: Card
+    private lateinit var earthCard: Card
+    private lateinit var appleCardTranslation: String
+    private lateinit var appleCardGroup: String
+    private lateinit var appleCardTranscription: String
+    private lateinit var appleCardWord: String
+    private lateinit var cardDateFormatPattern: String
+    private lateinit var userCharSequenceTyped: String
+    private lateinit var planetGroupName: String
+    private lateinit var fruitGroupName: String
 
 
     @Before
     fun setupDatabaseEnvironment() {
         val databaseContext = ApplicationProvider.getApplicationContext<Application>()
+        val applicationResources = databaseContext.resources
         applicationDatabase = Room.inMemoryDatabaseBuilder(databaseContext, ApplicationDatabase::class.java).build()
+
         cardDao = applicationDatabase.getCardDao()
         dictionaryDao = applicationDatabase.getDictionaryDao()
         additionalTestCoroutineContext = Dispatchers.Default
+
+        userCharSequenceTyped = applicationResources.getString(R.string.user_char_sequence_typed)
+        cardDateFormatPattern = applicationResources.getString(R.string.card_date_format_pattern)
+
+        planetGroupName = applicationResources.getString(R.string.planet_group_name)
+        fruitGroupName = applicationResources.getString(R.string.fruit_group_name)
+
+        appleCardWord = applicationResources.getString(R.string.apple_card_word)
+        appleCardTranslation = applicationResources.getString(R.string.apple_card_translation)
+        appleCardGroup = applicationResources.getString(R.string.apple_card_group)
+        appleCardTranscription = applicationResources.getString(R.string.apple_card_transcription)
+
+        appleCard = Card(id = applicationResources.getInteger(R.integer.apple_card_id),
+                         word = appleCardWord,
+                         translation = appleCardTranslation,
+                         group = appleCardGroup,
+                         transcription = appleCardTranscription,
+                         isDisplayed = applicationResources.getBoolean(R.bool.is_apple_card_displayed))
+
+        earthCard = Card(id = applicationResources.getInteger(R.integer.earth_card_id),
+                         word = applicationResources.getString(R.string.earth_card_word),
+                         translation = applicationResources.getString(R.string.earth_card_translation),
+                         transcription = applicationResources.getString(R.string.earth_card_transcription),
+                         group = planetGroupName,
+                         isDisplayed = false)
     }
 
 
@@ -48,13 +85,8 @@ class ApplicationDatabaseTester {
     fun testCardListExtraction() {
 
         runBlocking {
-            val fakeWord = "apple"
-            val fakeTranslation = "яблоко"
-            val isDisplayed = true
-            val fakeCard = Card(fakeWord, fakeTranslation, isDisplayed = isDisplayed)
-            val fakeCardList = listOf(fakeCard)
-
-            val cardInsertionJob = launch(additionalTestCoroutineContext) { cardDao.insertCard(fakeCard) }
+            val fakeCardList = listOf(appleCard)
+            val cardInsertionJob = launch(additionalTestCoroutineContext) { cardDao.insertCard(appleCard) }
             cardInsertionJob.join()
             val cardList = withContext(additionalTestCoroutineContext) { cardDao.getCardList() }
             assertEquals(true, cardList == fakeCardList)
@@ -65,13 +97,12 @@ class ApplicationDatabaseTester {
     fun testWordMapExtraction() {
 
         runBlocking {
-            val word = "apple"
-            val date = LocalDate.now().format(DateTimeFormatter.ofPattern("d.MM.yyyy"))
-            val postInsertionWordMap = mapOf(word to date)
+            val date = LocalDate.now().format(DateTimeFormatter.ofPattern(cardDateFormatPattern))
+            val postInsertionWordMap = mapOf(appleCardWord to date)
             val deferredWordMap: Deferred<Map<String, String>>
 
             val wordMapInsertionJob
-                    = launch(additionalTestCoroutineContext) { cardDao.insertHistoryEntry(word, date) }
+                    = launch(additionalTestCoroutineContext) { cardDao.insertHistoryEntry(appleCardWord, date) }
             wordMapInsertionJob.join()
 
             deferredWordMap = async(additionalTestCoroutineContext) { cardDao.getWordMap() }
@@ -85,16 +116,13 @@ class ApplicationDatabaseTester {
     fun testCardGroupMapExtraction() {
 
         runBlocking {
+            val cardGroupTitle = appleCardGroup
             val deferredCardGroupMap: Deferred<Map<String, List<Card>>>
-            val cardGroupTitle = "Fruits"
-            val appleCard = Card("apple", "яблоко", cardGroupTitle)
-            val orangeCard = Card("orange", "апельсин", cardGroupTitle)
-            val fruitCardList = listOf(appleCard, orangeCard)
+            val fruitCardList = listOf(appleCard)
             val fruitCardGroupMap: Map<String, List<Card>> = mapOf(cardGroupTitle to fruitCardList)
 
             val wordMapInsertionJob = launch(additionalTestCoroutineContext) {
                 cardDao.insertCard(appleCard)
-                launch { cardDao.insertCard(orangeCard) }
             }
             wordMapInsertionJob.join()
 
@@ -107,7 +135,6 @@ class ApplicationDatabaseTester {
 
     @Test
     fun testWordListExtraction() {
-        val userCharSequenceTyped = "ab"
         val deferredWordList: Deferred<List<String>>
 
         runBlocking {
@@ -122,13 +149,12 @@ class ApplicationDatabaseTester {
 
     @Test
     fun testTranslationListExtraction() {
-        val word = "apple"
         val deferredTranslationList: Deferred<List<String>>
 
         runBlocking {
 
             deferredTranslationList = async(additionalTestCoroutineContext) {
-                dictionaryDao.getTranslationList(word)
+                dictionaryDao.getTranslationList(appleCardWord)
             }
 
             assertEquals(false, deferredTranslationList.await().isEmpty())
@@ -137,30 +163,26 @@ class ApplicationDatabaseTester {
 
     @Test
     fun testTranscriptionExtraction() {
-        val word = "apple"
-        val wordTranscriptionToCompare = "[ˈæpəl]"
         val deferredTranscription: Deferred<String>
 
         runBlocking {
 
             deferredTranscription = async(additionalTestCoroutineContext) {
-                dictionaryDao.getTranscription(word)
+                dictionaryDao.getTranscription(appleCardWord)
             }
 
-            assertEquals(wordTranscriptionToCompare, deferredTranscription.await())
+            assertEquals(appleCardTranscription, deferredTranscription.await())
         }
     }
 
     @Test
     fun testExampleListExtraction() {
-        val word = "apple"
-        val translation = "яблоко"
         val deferredExampleList: Deferred<List<String>>
 
         runBlocking {
 
             deferredExampleList = async(additionalTestCoroutineContext) {
-                dictionaryDao.getExampleList(word, translation)
+                dictionaryDao.getExampleList(appleCardWord, appleCardTranslation)
             }
 
             assertEquals(false, deferredExampleList.await().isEmpty())
@@ -169,10 +191,6 @@ class ApplicationDatabaseTester {
 
     @Test
     fun testGroupTitleListExtraction() {
-        val planetGroupName = "Planets"
-        val fruitGroupName = "Fruits"
-        val appleCard = Card(word = "apple", translation = "яблоко", group = planetGroupName)
-        val earthCard = Card(word = "Earth", translation = "Земля", group = fruitGroupName)
         val deferredGroupTitleList: Deferred<List<String>>
         val groupTitleListToCompare = listOf(planetGroupName, fruitGroupName)
 
