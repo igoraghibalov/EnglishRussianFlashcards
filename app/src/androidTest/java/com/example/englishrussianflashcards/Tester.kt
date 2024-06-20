0177654f0d1b71d07b4d2bc542034649822a388c
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-20 22:34:41 +0300

    New feature: add Tester abstract class

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/Tester.kt
@@ -0,0 +1,13 @@
+package com.example.englishrussianflashcards
+
+/**
+ * Created by Igor Aghibalov on 20.06.2024
+ */
+abstract class Tester: TestEnvironmentSetuper, CaseTester {
+
+
+    override fun testCase(caseTestHandler: CaseUiTestHandler) {
+        caseTestHandler.handleCaseTest()
+        caseTestHandler.doAfterCaseTestHandling()
+    }
+}
