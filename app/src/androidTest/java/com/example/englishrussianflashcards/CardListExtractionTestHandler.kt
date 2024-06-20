9b948d4bd537c442cf4b302d11bd8fb6c2c486f5
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-20 22:58:50 +0300

    New feature: add CardListExtractionTestHandler class

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/CardListExtractionTestHandler.kt
@@ -0,0 +1,31 @@
+package com.example.englishrussianflashcards
+
+import com.example.englishrussianflashcards.domain.Card
+import com.example.englishrussianflashcards.domain.CardDao
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.junit.Assert
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * Created by Igor Aghibalov on 19.06.2024
+ */
+class CardListExtractionTestHandler(private val card: Card,
+                                    private val additionalTestCoroutineContext: CoroutineContext,
+                                    private val cardDao: CardDao): CaseTestHandler {
+
+
+    override fun handleCaseTest() {
+
+        runBlocking {
+            val expectedCardList = listOf(card)
+            val cardInsertionJob = launch(additionalTestCoroutineContext) { cardDao.insertCard(card) }
+            cardInsertionJob.join()
+            val cardList = withContext(additionalTestCoroutineContext) { cardDao.getCardList() }
+            Assert.assertEquals(expectedCardList, cardList)
+        }
+    }
+
+    override fun doAfterCaseTestHandling() {}
+}
