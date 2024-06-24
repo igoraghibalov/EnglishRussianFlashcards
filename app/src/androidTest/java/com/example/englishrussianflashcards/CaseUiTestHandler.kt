325c418df02a67531f8b338ebabe34a8a800a6f5
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-24 23:03:02 +0300

    Refactoring: implement ScreenRotator interface by CaseUiTestHandler abstract class

--- app/src/androidTest/java/com/example/englishrussianflashcards/CaseUiTestHandler.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/CaseUiTestHandler.kt
@@ -4,14 +4,16 @@ import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
 
 /**
  * Created by Igor Aghibalov on 16.06.2024
  */
 abstract class CaseUiTestHandler: ViewClickHandler,
                                   ViewAppearanceTester,
-                                  CaseTestHandler {
-
+                                  CaseTestHandler,
+                                  ScreenRotator {
     override fun clickOnView(viewId: Int) {
         Espresso.onView(ViewMatchers.withId(viewId))
             .perform(ViewActions.click())
@@ -20,5 +22,9 @@ abstract class CaseUiTestHandler: ViewClickHandler,
     override fun checkViewAppearance(viewId: Int) {
         Espresso.onView(ViewMatchers.withId(viewId))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    override fun rotateScreen() {
+        UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).setOrientationLeft()
     }
 }
