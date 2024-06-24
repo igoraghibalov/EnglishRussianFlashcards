fe4f8348f366291b43d2d3685a7a51c462dcb58d
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-24 22:18:16 +0300

    New feature: add CardDemoDialogFragmentInflationTestHandler class

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/casetesthandlers/CardDemoDialogFragmentInflationTestHandler.kt
@@ -0,0 +1,27 @@
+package com.example.englishrussianflashcards.casetesthandlers
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import com.example.englishrussianflashcards.CaseUiTestHandler
+import com.example.englishrussianflashcards.R
+
+/**
+ * Created by Igor Aghibalov on 24.06.2024
+ */
+class CardDemoDialogFragmentInflationTestHandler: CaseUiTestHandler() {
+
+
+    override fun handleCaseTest() {
+        onView(ViewMatchers.withId(R.id.card_creation_button))
+            .perform(ViewActions.click())
+
+        onView(ViewMatchers.withId(R.id.card_front_side_view))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+
+    override fun doAfterCaseTestHandling() {}
+}
