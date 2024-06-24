52020b46db4a1b710d46d0084c66aae7022b44d9
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-24 23:06:15 +0300

    New feature : add CardCreationFragmentOnScreenRotationTestHandler class

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/casetesthandlers/CardCreationFragmentOnScreenRotationTestHandler.kt
@@ -0,0 +1,33 @@
+package com.example.englishrussianflashcards.casetesthandlers
+
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.example.englishrussianflashcards.CaseUiTestHandler
+import com.example.englishrussianflashcards.R
+
+/**
+ * Created by Igor Aghibalov on 24.06.2024
+ */
+class CardCreationFragmentOnScreenRotationTestHandler(private val defaultTypingText: String)
+    : CaseUiTestHandler() {
+
+
+    override fun handleCaseTest() {
+        onView(withId(R.id.word_typing_text_view))
+            .perform(typeText(defaultTypingText))
+
+        rotateScreen()
+
+        onView(withId(R.id.word_typing_text_view))
+            .check(matches(withText(defaultTypingText)))
+    }
+
+    override fun doAfterCaseTestHandling() {}
+}
