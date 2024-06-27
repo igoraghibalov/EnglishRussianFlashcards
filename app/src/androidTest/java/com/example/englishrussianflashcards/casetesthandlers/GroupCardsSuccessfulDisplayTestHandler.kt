1ebc474e85ebcb06b3b423b1d9ad09363e03ac7d
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-27 23:53:50 +0300

    Refactoring: 1) move out group card presence check action; 2) complement handleCaseTest() method in a GroupCardsSuccessfulDisplayTestHandler class

--- app/src/androidTest/java/com/example/englishrussianflashcards/casetesthandlers/GroupCardsSuccessfulDisplayTestHandler.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/casetesthandlers/GroupCardsSuccessfulDisplayTestHandler.kt
@@ -6,12 +6,12 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.matcher.ViewMatchers
 import com.example.englishrussianflashcards.CaseUiTestHandler
+import com.example.englishrussianflashcards.GroupCardPresenceCheckAction
 import com.example.englishrussianflashcards.R
 import com.example.englishrussianflashcards.domain.ApplicationDatabase
 import com.example.englishrussianflashcards.domain.Card
@@ -24,32 +24,33 @@ import org.hamcrest.Matchers
 /**
  * Created by Igor Aghibalov on 26.06.2024
  */
-/*TODO:
-         * - Insert 2 fruit cards
-         * - click cards button in a main menu
-         * - assert: grid element contains fruit group name and 2 number - fruit cards count
-         * - test ui behaviour on database connection fail
- */
+//TODO: test ui behaviour on database connection fail
 class GroupCardsSuccessfulDisplayTestHandler(private val applicationContext: Context): CaseUiTestHandler() {
 
 
     override fun handleCaseTest() {
-        val applicationDatabase = Room.inMemoryDatabaseBuilder(applicationContext, ApplicationDatabase::class.java)
-            .build()
+        val groupName = "Fruits"
+        val  groupElementsCount = 2
+        val applicationDatabase = Room.inMemoryDatabaseBuilder(applicationContext,
+                                                               ApplicationDatabase::class.java)
+                                      .build()
 
         val cardDao = applicationDatabase.getCardDao()
+
+        val groupCardRecyclerViewInteraction = onView(ViewMatchers.withId(R.id.card_group_recycler_view))
+        val groupCardPresenceCheckAction = GroupCardPresenceCheckAction(groupName, groupElementsCount)
 
         val appleCard = Card(id = 0,
             word = "apple",
             translation = "яблоко",
             transcription = "[ˈæpəl]",
-            group = "Fruits",
+            group = groupName,
             isDisplayed = false)
         val orangeCard = Card(id = 1,
             word = "orange",
             translation = "апельсин",
             transcription = "[ˈɔːrəndʒ]",
-            group = "Fruits",
+            group = groupName,
             isDisplayed = false)
 
         runBlocking {
@@ -61,58 +62,11 @@ class GroupCardsSuccessfulDisplayTestHandler(private val applicationContext: Con
             }
 
             cardInsertionJob.join()
-
         }
-
-
+        clickOnView(R.id.cards_button)
+        groupCardRecyclerViewInteraction.perform(groupCardPresenceCheckAction)
     }
 
 
     override fun doAfterCaseTestHandling() {}
-
-
-    fun isGroupCardPresent(groupName: String,
-                           groupElementsCount: Int): Boolean {
-        var isGroupCardPresent: Boolean = false
-        val cardGroupRecyclerViewInteraction = onView(ViewMatchers.withId(R.id.card_group_recycler_view))
-
-        val groupCardPresenceCheckAction = object: ViewAction {
-
-            override fun getDescription(): String =
-                "Performing group card presence check action"
-
-
-            override fun getConstraints(): Matcher<View> {
-                return Matchers.allOf(
-                    ViewMatchers.isAssignableFrom(RecyclerView::class.java),
-                    ViewMatchers.isDisplayed()
-                )
-            }
-
-
-            override fun perform(uiController: UiController?, view: View?) {
-                val recyclerView = view as RecyclerView
-                val layoutManager = recyclerView.layoutManager
-                val childCount = layoutManager!!.childCount
-                var childView: CardView
-
-                for (childIndex in 0 until childCount) {
-                    childView =
-                        layoutManager.getChildAt(childIndex)!! as CardView
-                    val groupNameView =
-                        childView.findViewById<TextView>(R.id.word_group_name_text_view)
-                    val wordCountView =
-                        childView.findViewById<TextView>(R.id.word_count_text_view)
-                    if (groupNameView.text == groupName
-                        && wordCountView.text == groupElementsCount.toString()
-                    ) {
-                        isGroupCardPresent = true
-                        break
-                    }
-                }
-            }
-        }
-        cardGroupRecyclerViewInteraction.perform(groupCardPresenceCheckAction)
-        return isGroupCardPresent
-    }
 }
