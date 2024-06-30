4b713e4b04fac7d249473fccbdfb61cadeb45738
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-06-30 13:49:03 +0300

    Refactoring: 1)Use ScrollPositionRetentionOnScreenRotationTestHandler; 2)Remove incorrect process death testing in a CardGroupsScreenUiTester class

--- app/src/androidTest/java/com/example/englishrussianflashcards/CardGroupsScreenUiTester.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/CardGroupsScreenUiTester.kt
@@ -1,14 +1,13 @@
 package com.example.englishrussianflashcards
 
 import com.example.englishrussianflashcards.casetesthandlers.GroupCardsSuccessfulDisplayTestHandler
+import com.example.englishrussianflashcards.casetesthandlers.ScrollPositionRetentionOnScreenRotationTestHandler
 import com.example.englishrussianflashcards.casetesthandlers.WordGroupFragmentInflationTestHandler
 import org.junit.Test
 
 /**
  * Created by Igor Aghibalov on 25.04.2024
  */
-//TODO: 1) create testGridScrollPositionRetention() method;
-//      2) refactor process death and screen rotation tests
 class CardGroupsScreenUiTester: UiTester() {
 
 
@@ -23,20 +22,8 @@ class CardGroupsScreenUiTester: UiTester() {
         testCase(caseTestHandler = WordGroupFragmentInflationTestHandler())
     }
 
-
     @Test
-    fun testCardsFragmentInflationAfterProcessDeath() {
-        setupProcessDeathTestEnvironment()
-        testFragmentInflation(R.id.cards_button, R.id.card_group_recycler_view)
-        mainActivityScenario.recreate()
-        checkViewAppearance(R.id.card_group_recycler_view)
-    }
-
-
-    @Test
-    fun testCardsFragmentInflationAfterDeviceRotation() {
-        testFragmentInflation(R.id.cards_button, R.id.card_group_recycler_view)
-        rotateScreen()
-        checkViewAppearance(R.id.card_group_recycler_view)
+    fun testGridScrollPositionRetention() {
+        testCase(caseTestHandler = ScrollPositionRetentionOnScreenRotationTestHandler(applicationContext))
     }
 }
