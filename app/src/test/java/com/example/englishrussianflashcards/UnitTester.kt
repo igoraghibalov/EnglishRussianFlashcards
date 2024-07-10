51246ae5b741dd7d061f0ade67aa14bcd03b6f87
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-07-10 23:23:37 +0300

    New feature: add UnitTester abstract class

---
+++ app/src/test/java/com/example/englishrussianflashcards/UnitTester.kt
@@ -0,0 +1,12 @@
+package com.example.englishrussianflashcards
+
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/**
+ * Created by Igor Aghibalov on 10.07.2024
+ */
+@RunWith(JUnit4::class)
+abstract class UnitTester: Tester() {
+
+}
