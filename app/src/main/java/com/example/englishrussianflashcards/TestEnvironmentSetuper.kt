1d44254c28aa602c85bebddd00a7718118ef9d4d
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-07-10 23:22:41 +0300

    Docs: move respective testing classes from androidTest module to main one

--- app/src/main/java/com/example/englishrussianflashcards/TestEnvironmentSetuper.kt
+++ app/src/main/java/com/example/englishrussianflashcards/TestEnvironmentSetuper.kt
@@ -1,8 +1,11 @@
 package com.example.englishrussianflashcards
 
+import org.junit.Before
+
 /**
  * Created by Igor Aghibalov on 16.06.2024
  */
 interface TestEnvironmentSetuper {
+    @Before
     fun setupTestEnvironment()
 }
