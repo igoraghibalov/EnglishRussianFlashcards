f63b57569142de23ce224c7bc3e8eaa0cfefe5ab
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-07-11 23:48:14 +0300

    Refactoring: append common properties to ViewModelTester abstract class replacing 'lateinit var' with "special case" objects

--- app/src/test/java/com/example/englishrussianflashcards/ViewModelTester.kt
+++ app/src/test/java/com/example/englishrussianflashcards/ViewModelTester.kt
@@ -1,12 +1,18 @@
 package com.example.englishrussianflashcards
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.englishrussianflashcards.domain.Repository
 import org.junit.Rule
 
 /**
  * Created by Igor Aghibalov on 10.05.2024
  */
 abstract class ViewModelTester: UnitTester() {
+    protected var liveDataWrapper: LiveDataWrapper<*> = LiveDataWrapper.DefaultLiveDataWrapper()
+    protected var repository: Repository = DefaultRepository()
+    protected var viewModel: FlashcardsAppViewModel = DefaultViewModel()
 
     @get: Rule
     val ruleToUpdateLiveDataValueOnPureJVM = InstantTaskExecutorRule()
