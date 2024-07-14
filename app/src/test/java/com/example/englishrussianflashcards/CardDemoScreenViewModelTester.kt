e38f5e87a971879bcae06d617f3ca3b0b8bf1f21
Igor Aghibalov <igor.aghibalov@gmail.com>
2024-07-14 14:17:38 +0300

    New feature: add CardDemoScreenViewModelTester class

---
+++ app/src/test/java/com/example/englishrussianflashcards/CardDemoScreenViewModelTester.kt
@@ -0,0 +1,40 @@
+package com.example.englishrussianflashcards
+
+import android.database.sqlite.SQLiteException
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+/**
+ * Created by Igor Aghibalov on 14.07.2024
+ */
+class CardDemoScreenViewModelTester: ViewModelTester() {
+    private lateinit var demoCard: Card
+
+
+    override fun setupTestEnvironment() {
+        demoCard = Card("apple")
+        liveDataWrapper = CardDemoLiveDataWrapper<UiState>()
+    }
+
+    @Test
+    fun testDemoCardExtractionSuccess() {
+        val successResult = UiState.Success(demoCard)
+        repository = SuccessDemoCardRepository(demoCard)
+        viewModel = CardDemoScreenViewModel(repository, liveDataWrapper)
+        viewModel.fetchDemoCard()
+        assertTrue("Demo card extraction failure", viewModel.isExtractionResultPresent(successResult))
+    }
+
+
+    @Test
+    fun testDemoCardExtractionFailure() {
+        val failureResult = UiState.Error()
+
+        // TODO: repository must throw exception on demoCard extraction
+        repository = FailureDemoCardRepository()
+        viewModel = CardDemoScreenViewModel(repository, liveDataWrapper)
+        viewModel.fetchDemoCard()
+        assertTrue("Demo card extraction exception handling failure",
+                   viewModel.isExtractionResultPresent(failureResult))
+    }
+}
