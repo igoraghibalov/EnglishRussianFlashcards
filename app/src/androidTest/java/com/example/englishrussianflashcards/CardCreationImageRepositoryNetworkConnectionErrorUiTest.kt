83a3973e8a76941c33f55ecdeca7a5b5abb3b8f8
Igor Aghibalov <igor.aghibalov@gmail.com>
2025-09-14 16:29:54 +0300

    create CardCreationImageRepositoryNetworkConnectionErrorUiTest class

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/CardCreationImageRepositoryNetworkConnectionErrorUiTest.kt
@@ -0,0 +1,83 @@
+package com.example.englishrussianflashcards
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.junit.Test
+
+/**
+ * Created by Igor Aghibalov on 12.09.2025
+ */
+@UninstallModules(DictionaryRepositoryModule::class, ImageRepositoryModule::class)
+@HiltAndroidTest
+class CardCreationImageRepositoryNetworkConnectionErrorUiTest: UiTest() {
+
+
+    @Module
+    @InstallIn(ViewModelComponent::class)
+    class ImageRepositoryNetworkConnectionErrorTestModule {
+
+        @Provides
+        @ViewModelScoped
+        fun provideNetworkConnectionErrorImageRepository(): ImageRepository {
+            return NetworkConnectionErrorImageRepository()
+        }
+    }
+
+
+
+    @Module
+    @InstallIn(ViewModelComponent::class)
+    class DictionaryRepositorySuccessTestModule {
+
+        @Provides
+        @ViewModelScoped
+        fun provideSuccessDictionaryRepository(): DictionaryRepository {
+            return SuccessDictionaryRepository()
+        }
+    }
+
+
+    override fun setUp() {
+        hiltRule.inject()
+    }
+
+
+    @Test
+    fun testImageSelectionOnNetworkConnectionFix() {
+        val cardCreationScreen = CardCreationScreen()
+        val typingWordCharacters = "ap"
+
+        with(cardCreationScreen) {
+            enableAirplaneMode()
+            typeWord(typingWordCharacters)
+            selectWordFromDropdownMenu(wordPosition = 0)
+            selectImage(imagePosition = 0)
+            checkNetworkConnectionErrorDialogPresence()
+            disableAirplaneMode()
+            selectImage(imagePosition = 0)
+            checkImageSelection()
+        }
+    }
+
+
+    @Test
+    fun testImageSelectionOnNetworkConnectionError() {
+        val cardCreationScreen = CardCreationScreen()
+        val typingWordCharacters = "ap"
+
+        with(cardCreationScreen) {
+            enableAirplaneMode()
+            typeWord(typingWordCharacters)
+            selectWordFromDropdownMenu(wordPosition = 0)
+            selectImage(imagePosition = 0)
+            checkNetworkConnectionErrorDialogPresence()
+            wait(timeToWait = 10000)
+            checkImageSelectionSkipDialogPresence()
+        }
+    }
+}
