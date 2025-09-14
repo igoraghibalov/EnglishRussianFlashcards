596ea1ddda5f12a2a48d0a8bb8a65001602fcfbe
Igor Aghibalov <igor.aghibalov@gmail.com>
2025-09-14 17:25:02 +0300

    delete useless files

--- app/src/androidTest/java/com/example/englishrussianflashcards/testers/InflationOnClickTester.kt
+++
@@ -1,8 +0,0 @@
-package com.example.englishrussianflashcards.testers
-
-import com.example.englishrussianflashcards.ViewClickHandler
-
-/**
- * Created by Igor Aghibalov on 26.07.2024
- */
-interface InflationOnClickTester: ViewAppearanceTester, ViewClickHandler
--- app/src/androidTest/java/com/example/englishrussianflashcards/testers/ViewAppearanceTester.kt
+++
@@ -1,8 +0,0 @@
-package com.example.englishrussianflashcards.testers
-
-/**
- * Created by Igor Aghibalov on 15.06.2024
- */
-interface ViewAppearanceTester {
-    fun checkViewAppearance(viewId: Int)
-}
