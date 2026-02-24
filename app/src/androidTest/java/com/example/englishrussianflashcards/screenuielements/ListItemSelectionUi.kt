0caf4d8ac278f268ab8201168ff6db2648ffcadb
Igor Aghibalov <igor.aghibalov@gmail.com>
2026-02-24 23:13:53 +0300

    tdd, ui-testing: create EspressoListItemSelectionUi class

--- app/src/androidTest/java/com/example/englishrussianflashcards/screenuielements/ListItemSelectionUi.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/screenuielements/ListItemSelectionUi.kt
@@ -5,7 +5,7 @@ import com.example.englishrussianflashcards.ViewDataExtraction
 /**
  * Created by Igor Aghibalov on 03.02.2026
  */
-interface ListItemSelectionUi: ViewDataExtraction<String> {
+interface ListItemSelectionUi<T: Any>: ViewDataExtraction<T> {
     fun showList()
     fun selectItem()
 }
---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/screenuielements/espresso/EspressoListItemSelectionUi.kt
@@ -0,0 +1,31 @@
+package com.example.englishrussianflashcards.screenuielements.espresso
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.example.englishrussianflashcards.ListItemSelectionTask
+import com.example.englishrussianflashcards.ViewDataExtraction
+import com.example.englishrussianflashcards.screenuielements.ListItemSelectionUi
+
+/**
+ * Created by Igor Aghibalov on 23.02.2026
+ */
+class EspressoListItemSelectionUi<T: Any>(private val listShowTriggerId: Int,
+                                          private val listItemSelectionTask: ListItemSelectionTask,
+                                          private val viewDataExtraction: ViewDataExtraction<T>)
+    : ListItemSelectionUi<T> {
+
+
+
+    override fun showList() {
+        onView(withId(listShowTriggerId)).perform(click())
+    }
+
+    override fun selectItem() {
+        listItemSelectionTask.run()
+    }
+
+    override fun extractViewData(): T {
+        return viewDataExtraction.extractViewData()
+    }
+}
