fb5c13c8e8f7059399bf93c91dcd53c90f61c265
Igor Aghibalov <igor.aghibalov@gmail.com>
2026-02-27 04:52:56 +0300

    tdd, ui-testing: refactor TextUi interface

--- app/src/androidTest/java/com/example/englishrussianflashcards/screenuielements/TextUi.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/screenuielements/TextUi.kt
@@ -1,11 +1,10 @@
 package com.example.englishrussianflashcards.screenuielements
 
 import com.example.englishrussianflashcards.ViewDataExtraction
-import org.hamcrest.Matcher
 
 /**
  * Created by Igor Aghibalov on 04.10.2025
  */
 interface TextUi<T: Any>: ViewDataExtraction<T> {
-    fun hasText(stringMatcher: Matcher<String>)
+    fun hasText()
 }
