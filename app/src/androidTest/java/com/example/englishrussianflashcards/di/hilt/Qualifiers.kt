b67f5e3b814145298ce65990976b27769f04c264
Igor Aghibalov <igor.aghibalov@gmail.com>
2026-03-07 17:28:25 +0300

    tdd, ui-testing: create hilt Qualifiers.kt file

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/di/hilt/Qualifiers.kt
@@ -0,0 +1,12 @@
+package com.example.englishrussianflashcards.di.hilt
+
+import javax.inject.Qualifier
+
+/**
+ * Created by Igor Aghibalov on 01.11.2025
+ */
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class EspressoWordSelectionUi
+
+
