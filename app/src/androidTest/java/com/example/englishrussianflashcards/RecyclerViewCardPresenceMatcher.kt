efb3ccb51ee2fe962cb21dd1d723c7f38c0cec13
Igor Aghibalov <igor.aghibalov@gmail.com>
2026-07-11 16:19:00 +0300

    tdd, ui-testing: refactor RecyclerViewCardPresenceMatcher class

--- app/src/androidTest/java/com/example/englishrussianflashcards/RecyclerViewCardPresenceMatcher.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/RecyclerViewCardPresenceMatcher.kt
@@ -3,39 +3,45 @@ package com.example.englishrussianflashcards
 import android.view.View
 import android.view.ViewParent
 import androidx.cardview.widget.CardView
+import com.example.englishrussianflashcards.createcard.presentation.databinding.ListItemCardLayoutBinding
 import org.hamcrest.Description
 import org.hamcrest.TypeSafeMatcher
+
+private const val CARD_LAYOUT_PARENT_VIEW_NAME = "MaterialCardView"
 
 /**
  * Created by Igor Aghibalov on 13.06.2026
  */
-class RecyclerViewCardPresenceMatcher<T: View>(private val card: Card): TypeSafeMatcher<T>(CardView::class.java) {
+class RecyclerViewCardPresenceMatcher<T: View>(private val card: Card)
+            : TypeSafeMatcher<T>(CardView::class.java) {
 
     override fun matchesSafely(cardView: T?): Boolean {
-        val rootView = provideRootView(cardView!!)
+        val rootView = provideCardLayoutParentView(cardView!!)
         val listItemCardLayoutBinding = ListItemCardLayoutBinding.bind(rootView)
 
         return with(listItemCardLayoutBinding) {
-                    word == card.word &&
-                    transcription == card.transcription &&
-                    translation == card.translation &&
-                    example == card.example &&
-                    imageTag == card.imageTag &&
-                    cardGroupName == card.cardGroupName
+                    word.text == card.word &&
+                    transcription.text == card.transcription &&
+                    translation.text == card.translation &&
+                    example.text == card.example &&
+                    image.tag == card.imageTag &&
+                    cardGroupName.text == card.cardGroupName
         }
     }
+
 
     override fun describeTo(description: Description?) {
-        description!!.appendText("$card is present in a recyclerView")
+        description!!.appendText("matches card presence in a recyclerView")
     }
 
-    fun provideRootView(view: T): ViewParent {
-        val rootViewName = "com.android.internal.policy.DecorView"
+
+    fun provideCardLayoutParentView(view: T): View {
+        val cardLayoutParentViewName = view.resources.getString(R.string.card_layout_parent_view_name)
         var viewParent: ViewParent = view.parent
 
-        while (viewParent.javaClass.name != rootViewName) {
+        while (!viewParent.javaClass.name.contains(cardLayoutParentViewName, true)) {
             viewParent = viewParent.parent
         }
-        return viewParent
+        return viewParent as View
     }
 }
