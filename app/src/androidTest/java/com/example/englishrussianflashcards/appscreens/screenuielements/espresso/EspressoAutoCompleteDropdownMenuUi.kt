289bd89ac83217a8d75ccc8742311bf6d52c6048
Igor Aghibalov <igor.aghibalov@gmail.com>
2026-07-12 18:16:59 +0300

    tdd, ui-testing: refactor EspressoAutoCompleteDropdownMenuUi class

---
+++ app/src/androidTest/java/com/example/englishrussianflashcards/appscreens/screenuielements/espresso/EspressoAutoCompleteDropdownMenuUi.kt
@@ -0,0 +1,44 @@
+package com.example.englishrussianflashcards.appscreens.screenuielements.espresso
+
+import android.view.View
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAction
+import com.example.englishrussianflashcards.MenuItemSelectionTask
+import com.example.englishrussianflashcards.appscreens.screenuielements.AutoCompleteDropdownMenuUi
+import com.example.englishrussianflashcards.appscreens.screenuielements.ClickableUi
+import org.hamcrest.Matcher
+
+/**
+* Created by Igor Aghibalov on 07.02.2026
+*/
+class EspressoAutoCompleteDropdownMenuUi(private val dropdownMenuItemSelectionTask: MenuItemSelectionTask,
+                                         private val characterTypeAction: ViewAction,
+                                         private val autoCompleteDropdownMenuUiMatcher: Matcher<View>,
+                                         private val textClearingUi: ClickableUi)
+    : AutoCompleteDropdownMenuUi {
+
+
+    override fun typeCharacters() {
+        onView(autoCompleteDropdownMenuUiMatcher).perform(characterTypeAction)
+    }
+
+    override fun clearText() {
+        textClearingUi.click()
+    }
+
+    override fun showList() {
+        typeCharacters()
+    }
+
+    override fun selectItem() {
+        dropdownMenuItemSelectionTask.run()
+    }
+
+    override fun checkItemPresence() {
+        selectItem()
+    }
+
+    override fun scrollToItem() {
+        selectItem()
+    }
+}
