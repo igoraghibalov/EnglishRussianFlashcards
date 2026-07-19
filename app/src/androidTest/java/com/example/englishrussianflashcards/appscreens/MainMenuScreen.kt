b3b851c2906d4a9aeb5dce0a2c2f3e36ce8e67c3
Igor Aghibalov <igor.aghibalov@gmail.com>
2026-07-19 12:35:56 +0300

    tdd, ui-testing: refactor MainMenuScreen class

--- app/src/androidTest/java/com/example/englishrussianflashcards/appscreens/MainMenuScreen.kt
+++ app/src/androidTest/java/com/example/englishrussianflashcards/appscreens/MainMenuScreen.kt
@@ -1,22 +1,22 @@
 package com.example.englishrussianflashcards.appscreens
 
+import com.example.englishrussianflashcards.appscreens.screenuielements.ClickableUi
 import com.example.englishrussianflashcards.di.hilt.EspressoMainScreenCardsButtonUi
 import com.example.englishrussianflashcards.di.hilt.EspressoMainScreenContinueButtonUi
 import com.example.englishrussianflashcards.di.hilt.EspressoMainScreenHistoryButtonUi
 import com.example.englishrussianflashcards.di.hilt.EspressoMainScreenNewCardButtonUi
 import com.example.englishrussianflashcards.di.hilt.EspressoMainScreenQuitButtonUi
-import com.example.englishrussianflashcards.appscreens.screenuielements.ButtonUi
 import javax.inject.Inject
 
 /**
  * Created by Igor Aghibalov on 11.04.2026
  */
 class MainMenuScreen @Inject constructor(
-    @EspressoMainScreenContinueButtonUi private val continueButtonUi: ButtonUi,
-    @EspressoMainScreenCardsButtonUi private val cardsButtonUi: ButtonUi,
-    @EspressoMainScreenNewCardButtonUi private val newCardButtonUi: ButtonUi,
-    @EspressoMainScreenHistoryButtonUi private val historyButtonUi: ButtonUi,
-    @EspressoMainScreenQuitButtonUi private val quitButtonUi: ButtonUi) {
+    @EspressoMainScreenContinueButtonUi private val continueButtonUi: ClickableUi,
+    @EspressoMainScreenCardsButtonUi private val cardsButtonUi: ClickableUi,
+    @EspressoMainScreenNewCardButtonUi private val newCardButtonUi: ClickableUi,
+    @EspressoMainScreenHistoryButtonUi private val historyButtonUi: ClickableUi,
+    @EspressoMainScreenQuitButtonUi private val quitButtonUi: ClickableUi) {
 
     fun clickContinueButton() { continueButtonUi.click() }
     fun clickCardsButton() { cardsButtonUi.click() }
